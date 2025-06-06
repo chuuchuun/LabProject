b0a59f0d4433d332edab4ef3169398854894bf1e
Alla Krylova <148686842+chuuchuun@users.noreply.github.com>
2025-06-06 14:38:37 +0200

    implemetnted user service for getting the client using gRPC

---
+++ GrpcClientTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using LabProject.Grpc;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("https://localhost:7132");
+        var client = new UserService.UserServiceClient(channel);
+
+        var reply = await client.GetUserAsync(new GetUserRequest { Id = 1 });
+        Console.WriteLine($"Name: {reply.Name}, Email: {reply.Email}");
+    }
+}
